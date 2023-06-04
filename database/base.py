4651394f94a1f256419c688a8129becd042f3774
BenjaminHerrera <BenHerrera1044@outlook.com>
2023-06-03 23:14:52 -0700

    Placed DB connection specs into the config file

--- database/base.py
+++ database/base.py
@@ -6,8 +6,10 @@ class DataAccessBase:
     """Encapsulation of all database actions"""
     
     # Static Variable Create
-    CLIENT = pymongo.MongoClient('mongodb://localhost:27018/')
-    DB = CLIENT['Det025']
+    CLIENT = pymongo.MongoClient(
+        f'mongodb://{config.database.domain}:{config.database.port}/'
+    )
+    DB = CLIENT[config.database.db]
     USER_COL = DB['users']
     REGISTER_COL = DB['registerList']
     REQ_ARGS = config.neededArguments.DataAccess
