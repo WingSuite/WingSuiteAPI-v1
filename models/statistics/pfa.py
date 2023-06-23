5218bbd311fe3ce313e1fef06009c75548dffa7b
BenjaminHerrera <BenHerrera1044@outlook.com>
2023-06-23 14:04:01 -0700

    Applied PEP-8 Standardization to all documents

--- models/statistics/pfa.py
+++ models/statistics/pfa.py
@@ -4,17 +4,17 @@ from config.config import arguments
 from typing import Any
 
 
-class Pfa:
+class PFA:
     """Pfa model class"""
 
     # Static variable declaration
     REQ_ARGS = arguments.models.statistics
 
-    def __init__(self: "Pfa", **kwargs: Any) -> None:
+    def __init__(self: "PFA", **kwargs: Any) -> None:
         """Constructor for base unit"""
 
         # Check if kwargs has the minimum arguments
-        for arg in Pfa.REQ_ARGS.pfa.init:
+        for arg in PFA.REQ_ARGS.pfa.init:
             if arg not in kwargs:
                 return False
 
